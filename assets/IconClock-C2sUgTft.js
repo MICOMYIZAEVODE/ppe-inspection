ac18288f9bf542b73b1c31c9aca2a25a529cb2f4
MICOMYIZAEVODE <132274193+MICOMYIZAEVODE@users.noreply.github.com>
2026-06-15 08:52:35 +0200

    Add files via upload

---
+++ assets/IconClock-C2sUgTft.js
@@ -0,0 +1,1 @@
+import{c}from"./createReactComponent-CDYgvsq-.js";const o=[["path",{d:"M3 12a9 9 0 1 0 18 0a9 9 0 1 0 -18 0",key:"svg-0"}],["path",{d:"M9 12l2 2l4 -4",key:"svg-1"}]],a=c("outline","circle-check","CircleCheck",o);const e=[["path",{d:"M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0",key:"svg-0"}],["path",{d:"M12 7v5l3 3",key:"svg-1"}]],n=c("outline","clock","Clock",e);export{a as I,n as a};
